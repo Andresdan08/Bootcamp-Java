5727c676e26f9d1c8962ac25651093a7d19daad3
Andresdan08 <andresdan08@gmail.com>
2024-02-26 22:37:40 -0400

    A
    Ejercicios realizados

--- Variables.java
+++
@@ -1,8 +0,0 @@
-public class Main {
-    public static void main(String[] args) {
-        String nombre = "Andres";
-        Integer nroEquipo = 11;
-        String texto = "Hola, soy " + nombre + " del equipo nro. " + nroEquipo;
-        System.out.println(texto);
-    }
-}
